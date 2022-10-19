33eafd2293916550a6ae49cad8ed1fc946f81825
joaoo2005 <joaovitorantunes8832@gmail.com>
2022-10-18 21:47:52 -0300

    demo

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/clinete/v1/")
+public class Controller {
+
+    @Autowired
+    Repository repository;
+
+    @PostMapping
+    public Cliente creat(@RequestBody Cliente cliente){
+        Cliente clienteSaved = repository.save(cliente);
+        return clienteSaved;
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Optional<Cliente> getClineteById(@PathVariable Long id){
+        Optional<Cliente> clienteReturned = repository.findById(id);
+        return clienteReturned;
+    }
+}
